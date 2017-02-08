9751e366310c8e09dc136c79c3f0772f770f5f54
vicvans20 <vicvans20@gmail.com>
2017-02-07 21:14:11 -0600

    Add message types from firmata protocol.

---
+++ lib/archytax/protocol/message_types.ex
@@ -0,0 +1,17 @@
+defmodule Archytax.Protocol.MessageTypes do
+  defmacro __using__(_) do
+    quote do
+      # Message Types
+      @analog               0xE0 # LSB(bits 0-6)
+      @digital              0x90 # LSB(bits 0-6)
+      @report_analog_pin    0xC0 # disable/enable(0/1)
+      @report_digital_port  0xD0 # disable/enable(0/1)
+      @start_sysex          0xF0 # Start sysex
+      @pin_mode             0xF4 # Set pin mode
+      @set_digital_pin      0xF5 # 
+      @sysex_end            0xF7 # End sysex 
+      @protocol_version     0xF9 # first byte major version, second byte minor version
+      @system_reset         0xFF # 
+    end
+  end
+end
