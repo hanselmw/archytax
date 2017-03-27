915d8f8c6be4f7c2604fffa0163b5a0e769420b7
vicvans20 <vicvans20@gmail.com>
2017-03-27 16:51:16 -0600

    Improves code readability.

--- lib/archytax/protocol/sysex.ex
+++ lib/archytax/protocol/sysex.ex
@@ -53,27 +53,6 @@ defmodule Archytax.Protocol.Sysex do
   end
 
   def execute(<< @capability_response :: size(8), data :: binary >>) do
-
-    evaluate_pin_info = fn
-      (byte, {pin_map, {:mode, _anything}}) ->
-        new_map = Map.put(pin_map, byte, nil)
-        {new_map, {:resolution, byte}} # set new mode and instruct that next byte is this mode resolution
-      (byte, {pin_map, {:resolution, mode_key}} ) ->
-        new_map = Map.put(pin_map, mode_key, byte)
-        {new_map, {:mode, true}} # set resolution for mode and instruct next byte is mode byte
-    end
-
-    # Initialize pin mode to each pin and insert into pins list
-    insert_pin_map = fn
-      ({pin_list, pin_number}, pins_store) ->
-        pin = %{}
-        { pin_map, _anything } = Enum.reduce(pin_list, { %{}, {:mode, 0}}, evaluate_pin_info )
-        new_map = Map.put(pin, :supported_modes, pin_map)
-        new_map = Map.put(new_map, :mode, @unknown)
-        # [ new_map | pins_store ]
-        Map.put(pins_store, pin_number, new_map)
-    end
-
     pins_data = binary_part(data, 0, byte_size(data) - 1) # remove last << 127  >> byte
     pins_data = String.split(pins_data, << 0x7f >>) # 0x7f separate each pin data on the binary
 
@@ -82,32 +61,52 @@ defmodule Archytax.Protocol.Sysex do
       |> Stream.with_index(0)
     #OPTIMIZE add validator for supported modes according to firmata before setting mode
     # Acumulator is pin_map with a operation instruction
-    pins_information = Enum.reduce(pins_array, %{} , insert_pin_map )
+    pins_information = Enum.reduce(pins_array, %{} , &insert_capability_info/2 )
     {:capability_response, pins_information}
   end
 
   def execute(<< @analog_mapping_response :: size(8), data :: binary >>) do
-    # Initialize pin mode to each pin and insert into pins list
-    insert_pin_information = fn
-      ({byte, pin_number}, pins_store) ->
-        pin = case byte do
-          127 ->
-            %{analog_channel: nil} # doesn't support analog
-          analog_channel_val ->
-            %{analog_channel: analog_channel_val} # analog channel for pin_number
-        end
-        Map.put(pins_store, pin_number, pin) # update pins_store map
-    end
-
     pins_list = :binary.bin_to_list(data)
       |> Stream.with_index(0)
-    analog_information = Enum.reduce(pins_list, %{}, insert_pin_information)
+    analog_information = Enum.reduce(pins_list, %{}, &insert_analog_info/2)
     {:analog_response, analog_information}
   end
 
   def execute(<< unknown :: size(8), _data :: binary >>) do
     IO.puts "#{unknown} is not a recognized sysex command."
     {:unknown}
+  end
+
+  # Initialize pin mode to each pin and insert into pins list
+  def insert_capability_info({pin_list, pin_number}, pins_store) do
+    pin = %{}
+    { pin_map, _anything } = Enum.reduce(pin_list, { %{}, {:mode, 0}}, &eval_pin_capability/2 )
+    new_map = Map.put(pin, :supported_modes, pin_map)
+    new_map = Map.put(new_map, :mode, @unknown)
+    # [ new_map | pins_store ]
+    Map.put(pins_store, pin_number, new_map)
+  end
+
+  # Initialize pin mode to each pin and insert into pins list
+  def insert_analog_info({byte, pin_number}, pins_store) do
+    pin = case byte do
+      127 ->
+        %{analog_channel: nil} # doesn't support analog
+      analog_channel_val ->
+        %{analog_channel: analog_channel_val} # analog channel for pin_number
+    end
+    Map.put(pins_store, pin_number, pin) # update pins_store map
+  end
+
+  # Evaluate pin capability response for `pin` and set the mode and resolution in a Map%{:mode_number => resolution val }
+  def eval_pin_capability(byte, {pin_map, {:mode, _anything}}) do
+    new_map = Map.put(pin_map, byte, nil)
+    {new_map, {:resolution, byte}} # set new mode and instruct that next byte is this mode resolution
+  end
+
+  def eval_pin_capability(byte, {pin_map, {:resolution, mode_key}}) do
+    new_map = Map.put(pin_map, mode_key, byte)
+    {new_map, {:mode, true}} # set resolution for mode and instruct next byte is mode byte
   end
 
 end
