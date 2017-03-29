3abcd73f3ea757b5c62e2ed6997b09278ca50d6e
vicvans20 <vicvans20@gmail.com>
2017-03-29 17:36:40 -0600

    Reuse main update attribute.

--- lib/archytax/board.ex
+++ lib/archytax/board.ex
@@ -49,14 +49,14 @@ defmodule Archytax.Board do
 
   def update_analog_channel_value(pins_map, analog_channel, val) do
     pin = Enum.find_index(pins_map, fn({_key_number, pin_data}) -> pin_data[:analog_channel] == analog_channel end) # Find pin number for analog channel
-    new_pin_map = Map.put(pins_map[pin], :value, val)
-    Map.put(pins_map, pin, new_pin_map)
+    {:ok, new_pins_map} = update_pin_attribute(pins_map, pin, :value, val)
+    new_pins_map
   end
 
   def report_analog_pin(pins_map, analog_channel, report_value) do
     pin = Enum.find_index(pins_map, fn({_key_number, pin_data}) -> pin_data[:analog_channel] == analog_channel end)
-    new_pin_map = Map.put(pins_map[pin], :report, report_value)
-    Map.put(pins_map, pin, new_pin_map)
+    {:ok, new_pins_map} = update_pin_attribute(pins_map, pin, :report, report_value)
+    new_pins_map
   end
 
   def parse_digital_message(new_pins_map, _port, _port_value, 8) do
