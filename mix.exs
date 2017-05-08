65133aea3ee5eb67ba54ace443a558c71b97455c
vicvans20 <vicvans20@gmail.com>
2017-05-08 15:52:19 -0600

    Update semantic version.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Archytax.Mixfile do
 
   def project do
     [app: :archytax,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
